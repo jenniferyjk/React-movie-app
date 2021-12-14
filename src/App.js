9bb8f447605327bd25b9f8b0c631570193c11aad
jenniferyjk <jaysstar0310@gmail.com>
2021-12-14 18:25:45 +0900

    json data view

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie"; // ./ 는 현재 디렉토리라는 뜻
+import './App.css'
+
+class App extends React.Component {
+
+  
+
+  state={
+    isLoading:true,
+    movies:[],
+  };
+
+  //화살표 함수로 상태(state)값에 접근 가능
+  //새로운 메서드 만들기
+  getMovies= async ()=>{
+    const movies = await axios.get('https://yts.mx/api/v2/list_movies.json');
+    console.log(movies.data.data.movies);
+    this.setState({
+      movies:movies.data.data.movies,
+      isLoading:false,
+    });
+  }
+
+
+ componentDidMount() {
+   /* setTimeout(()=>{
+     this.setState({
+       isLoading:false,
+     });
+   },6000); */
+
+   // axios로 getmovies 메서드 호출
+   this.getMovies();
+ }
+
+
+  render(){
+    
+    return (
+        <section className="container"> 
+          {this.state.isLoading ? 
+          <div className="loader">
+            <span className="loader__text">
+              'Loading...'
+            </span>
+          </div> 
+          : (
+            <div className="movies">
+              {this.state.movies.map((item,index)=>{
+            //console.log(item);
+            return < Movie key={index} 
+            genres={item.genres} year={item.year} summary={item.summary} 
+            title={item.title} poster={item.medium_cover_image}/>
+          })}
+            </div>
+          )
+            
+             }
+        </section>
+      );
+    }
+  }
+  
 
 export default App;
